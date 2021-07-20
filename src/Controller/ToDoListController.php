fbb5139c3beb7da06d01d1e0c013a9249a16bc98
JessieProgrammer <2109882@talnet.nl>
2021-07-21 01:09:34 +0200

    mod2

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -4,10 +4,15 @@
     use App\Entity\Todolist;
 
     use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\Routing\Annotation\Route;
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-  
+    
+    use Symfony\Component\Form\Extension\Core\Type\TextType;
+    use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+    use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
     class ToDoListController extends AbstractController {
         /**
          * @Route("/")
@@ -21,12 +26,64 @@
         }
 
         /**
+         * @Route("/list/new", name="task_list")
+         * Method({"GET", "POST"})
+         */
+        public function new(Request $request) {
+            $todolist = new ToDoList();
+
+            $form = $this->createFormBuilder($todolist)
+        ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+        ->add('body', TextareaType::class, array(
+          'required' => false,
+          'attr' => array('class' => 'form-control')
+        ))
+        ->add('save', SubmitType::class, array(
+          'label' => 'Create',
+          'attr' => array('class' => 'btn btn-primary mt-3')
+        ))
+        ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $task = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('task_list');
+        }
+
+            return $this->render('list/new.html.twig', array(
+                'form' => $form->createView()
+            ));
+        }
+
+        /**
          * @Route("/list/{id}")
          */
         public function show($id) {
             $lists = $this->getDoctrine()->getRepository(ToDolist::Class)->find($id);
 
             return $this->render('list/show.html.twig', array('lists' => $lists));
+        }
+
+        /**
+         * @Route("list/delete/{id}")
+         * @Method({"DELETE"})
+         */
+        public function delete(Request $request, $id) {
+            $lists = $this->getDoctrine()->getRepository
+            (ToDoList::class)->find($id);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($lists);
+            $entityManager->flush();
+
+            $response = new Response();
+            $response->send();
         }
 
         // /**
